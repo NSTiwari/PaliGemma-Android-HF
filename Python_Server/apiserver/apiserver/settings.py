099ff31fc2acad4c71b9309dcd792c6935516b92
Sagar <85388413+Sagar0-0@users.noreply.github.com>
2024-07-15 16:41:56 +0530

    Added ngrok host

--- Python_Server/apiserver/apiserver/settings.py
+++ Python_Server/apiserver/apiserver/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure--fj9npypuorxv@(#y7q0)wza*d*0+_w#)492^cq3h_kazjk@' 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['2a64-152-58-103-208.ngrok-free.app']
 
 
 # Application definition
