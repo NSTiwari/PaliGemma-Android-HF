2da786f9e1c985d661bf62c0bc8f54ddaddb3b94
Nitin Tiwari <37960279+NSTiwari@users.noreply.github.com>
2024-07-29 21:36:54 +0530

    Update settings.py

--- Python_Server/apiserver/apiserver/settings.py
+++ Python_Server/apiserver/apiserver/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
