f2319aed51e6e92e517eceeb78a28424eb96fcfe
Nitin Tiwari <37960279+NSTiwari@users.noreply.github.com>
2024-07-29 22:17:04 +0530

    Update settings.py

--- Python_Server/apiserver/apiserver/settings.py
+++ Python_Server/apiserver/apiserver/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'apiserver.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'db.sqlite3',
     }
 }
 
