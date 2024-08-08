63e8bd5563fb424a2a0645d564d8065c944554ab
Nitin Tiwari <37960279+NSTiwari@users.noreply.github.com>
2024-08-08 13:13:41 +0530

    Added segmentation parsing using Big_Vision.

--- Python_Server/apiserver/api/views.py
+++ Python_Server/apiserver/api/views.py
@@ -1,30 +1,60 @@
 from ninja import NinjaAPI, File, UploadedFile, Form
 from gradio_client import Client, handle_file
 from PIL import Image
+import big_vision.evaluators.proj.paligemma.transfers.segmentation as segeval
+import numpy as np
 
 # from decouple import config     #if using your own token
 
 from .models import ImageDetection
 
 import pathlib
-
+import sys
 import os
 import re
 
 
 api = NinjaAPI()
+reconstruct_masks = segeval.get_reconstruct_masks('oi')
+
+if "big_vision_repo" not in sys.path:
+  sys.path.append("big_vision_repo")
 
 def normalize_coordinates(coord: str, img_x, img_y):
-    pattern = r'<loc(\d+)>'
-    matches = re.findall(pattern, coord)
-    # The coordinates also need to be normalized i.e., divide by 1024 and then multiple by height, width of the original image.
+    detect_pattern = r'<loc(\d+)>'
+    detect_matches = re.findall(detect_pattern, coord)
     
-    numbers = [int(match) for match in matches]
+    # Normalize the coordinates.
+    numbers = [int(detect_match) for detect_match in detect_matches]
     numbers[0] = int((numbers[0] / 1024) * img_y)
     numbers[1] = int((numbers[1] / 1024) * img_x)
     numbers[2] = int((numbers[2] / 1024) * img_y)
     numbers[3] = int((numbers[3] / 1024) * img_x)
+
     return numbers
+
+def parse_segmentation(coord: str, img_x, img_y):
+    detect_pattern = r'<loc(\d+)>'
+    detect_matches = re.findall(detect_pattern, coord)
+    
+    # Normalize the coordinates.
+    numbers = [int(detect_match) for detect_match in detect_matches]
+    numbers[0] = int((numbers[0] / 1024) * img_y)
+    numbers[1] = int((numbers[1] / 1024) * img_x)
+    numbers[2] = int((numbers[2] / 1024) * img_y)
+    numbers[3] = int((numbers[3] / 1024) * img_x)
+
+    # Parse segmentation key points.
+    segment_pattern = r'<seg(\d+)>'
+    segment_matches = re.findall(segment_pattern, coord)
+    segs = []
+    segments = [int(segment_match) for segment_match in segment_matches]
+    segs.append(segments)
+
+    mask = np.array(reconstruct_masks(np.array(segs)))
+
+    return numbers, mask[0]
+    
 
 @api.post('/detect')
 def detect(request, prompt: Form[str], image: File[UploadedFile], width: Form[int], height: Form[int]):
@@ -86,6 +116,34 @@ def detect(request, prompt: Form[str], image: File[UploadedFile], width: Form[in
                 temp["label"] = object["class_or_confidence"]
                 temp['coordinates'] = normalize_coordinates(object["token"], img_x, img_y)
                 container.append(temp)
+            elif prompt_word[0] == "segment":
+                bbox, seg_output = parse_segmentation(object["token"], img_x, img_y)
+                y1, x1, y2, x2 = bbox[0], bbox[1], bbox[2], bbox[3]
+                mask_width = x2 - x1
+                mask_height = y2 - y1
+                x_scale = mask_width / 64
+                y_scale = mask_height / 64
+
+                x_coords = np.arrange(mask_width)
+                y_coords = np.arrange(mask_height)
+                x_coords = (x_coords / x_scale).astype(int)
+                y_coords = (y_coords / y_scale).astype(int)
+                
+                resized_segment_mask = seg_output[y_coords[:, np.newaxis], x_coords]
+                resized_segment_mask = np.squeeze(resized_segment_mask)
+
+                x_mask = []
+                y_mask = []
+                for y in range(mask_height):
+                    for x in range(mask_width):
+                        if resized_segment_mask[y, x] > 0:
+                            x_mask.append(x)
+                            y_mask.append(y)
+
+                temp["label"] = object["class_or_confidence"]
+                temp["segment_x"] = x_mask
+                temp["segment_y"] = y_mask
+                return temp
             else:
                 temp["response"] = object["token"]
         if "detect" in prompt:
